95d6342a54d61922a7d53051376fce7de69db279
sunil <sunil@192.168.0.5>
2020-10-13 11:36:41 +0530

    sample testing

---
+++ src/Sample.java
@@ -0,0 +1,17 @@
+
+public class Sample {
+
+	public static void main(String[] args) {
+
+		int x = 15;
+		int y = 15;
+		
+		System.out.println((x > y) && (x == y));
+		System.out.println((x > y) || (x == y));
+		System.out.println(!(x == y));
+		
+
+
+	}
+
+}
